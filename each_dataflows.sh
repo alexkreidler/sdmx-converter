a3b34eddef072e4902836bcb1de7dbdb2b8efe4e
Alex Kreidler <alexkreidler2020@gmail.com>
2021-03-31 01:18:49 -0400

    final updates

--- each_dataflows.sh
+++ each_dataflows.sh
@@ -8,6 +8,9 @@ work_dir="/mnt/data/sdmx"
 
 # set -euxo
 
+mkdir -p $work_dir/errors
+mkdir -p $work_dir/timings
+
 for source in $(echo "${sources}" | jq -r '.[] | @base64'); do
     _jq() {
         echo ${source} | base64 --decode | jq -r ${1}
@@ -21,7 +24,8 @@ for source in $(echo "${sources}" | jq -r '.[] | @base64'); do
     base=$(_jq '.url')
 
     echo "$id DSDs"
-    flows=$(cat $out_dir/Dataflows.json | jq '.Structure.Structures.Dataflows.Dataflow | .[0:2]')
+    # | .[2:4]
+    flows=$(cat $out_dir/Dataflows.json | jq '.Structure.Structures.Dataflows.Dataflow')
 
     for flow in $(echo "${flows}" | jq -r '.[] | @base64'); do
         _jqi() {
@@ -42,7 +46,13 @@ for source in $(echo "${sources}" | jq -r '.[] | @base64'); do
         url="${base}datastructure/$u"
         echo "Dataflow $id, Datastructure $f from $url"
 
-        http GET "$url" >$out_dir/DataStructures/$f.xml
+        dl() {
+            http GET "$url" >$out_dir/DataStructures/$f.xml
+        }
+
+        name="${id}_${f}"
+
+        { time dl 2> $work_dir/errors/$name.log ; } 2> $work_dir/timings/$name.log
 
     done
     # .[0:10]
